4fef89d7355677149c2579f5079d6738d4ab7271
JYC11 <jaeyoon.c@gmail.com>
2023-06-07 18:03:37 +0900

    unholy parsing abomination(it's only gonna get worse from here)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,37 @@
 fun main(args: Array<String>) {
-    val input = readInput("day05-1")
-    println(input)
+    val input = readInput("day05").split("\n\n")
+    val rawCrates: List<String> = input[0]
+        .replace("    ", " [&]")
+        .split("\n")
+        .filter { !it.contains("1") }
+        .map { it.trim() }
+        .map { it.replace("][", "] [") }
+
+    val crates = mutableMapOf<Int, MutableList<String>>(
+        1 to mutableListOf<String>(),
+        2 to mutableListOf<String>(),
+        3 to mutableListOf<String>(),
+        4 to mutableListOf<String>(),
+        5 to mutableListOf<String>(),
+        6 to mutableListOf<String>(),
+        7 to mutableListOf<String>(),
+        8 to mutableListOf<String>(),
+        9 to mutableListOf<String>(),
+    )
+
+    for (crateRow in rawCrates) {
+        var i = 0
+        val temp = crateRow.split(" ")
+        while (i < 9 && i < temp.size) {
+            val crate = temp[i].replace("[", "").replace("]", "")
+            if (crate != "&") {
+                crates[i + 1]?.add(crate)
+            }
+            i += 1
+        }
+    }
+
+    crates.forEach { (key, value) ->
+        println("col: $key, crates: $value")
+    }
 }
